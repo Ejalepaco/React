f8a504e2592241299ddbc9ab3e391cf17db35d18
Ejalepaco <fdominguezp@gmail.com>
2025-06-15 09:37:25 +0200

    update

---
+++ usuarios-vcm/src/lib/db.ts
@@ -0,0 +1,9 @@
+// src/lib/db.ts
+import mysql from 'mysql2/promise';
+
+export const db = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'vcm',
+});
